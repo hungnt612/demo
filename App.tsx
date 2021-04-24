d5a709b4b3a2bf65c411d0a3cb602a1080d6975c
hungnt <hungnt@hungnts-iMac.local>
2021-04-25 01:54:52 +0700

    test

---
+++ App.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,a
+} from 'react-native/Libraries/NewAppScreen';
+import flatList from "./src/screen/flatList";
+import Tesst from "./src/screen/tesst";
+const App = () => {
+  return (
+    <Tesst></Tesst>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex:1,
+  },
+  itemContainer:{
+
+  },
+  imgContainer:{
+
+  },
+  titleContainer:{
+
+  },
+  priceContaine:{
+
+  },
+});
+
+export default App;
---
+++ src/screen/flexBox.tsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,a
+} from 'react-native/Libraries/NewAppScreen';
+
+const flatList = () => {
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.container}>
+        <View style={styles.container2} />
+        <View style={styles.container2} />
+        <View style={styles.container2} />
+        <View style={styles.container2} />
+      </View>
+      <View style={styles.container3}>
+        <View style={styles.container2} />
+        <View style={styles.container2} />
+        <View style={styles.container2} />
+      </View>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'column',
+  },
+  container3: {
+    flexDirection: 'row',
+  },
+  container2: {
+    width: 100,
+    height: 100,
+    backgroundColor: 'red',
+    marginVertical: 10,
+    marginHorizontal: 10,
+  },
+});
+
+export default flatList;

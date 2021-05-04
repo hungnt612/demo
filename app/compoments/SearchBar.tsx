1d76e3c20ce5ed41df1f3b7ed8ef8da42064ca7c
hungnt612 <tuanhung0601@live.com>
2021-05-04 15:04:08 +0700

    nuke

--- app/compoments/SearchBar.tsx
+++ app/compoments/SearchBar.tsx
@@ -1,1 +1,28 @@
-hihi
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+
+const SearchBar = () => {
+  return (
+    <SafeAreaView style={styles.container}>
+
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container:{
+    flex:1,
+  },
+});
+
+export default SearchBar;
+

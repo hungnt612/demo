0186d8af387af59999e5dc4b31ecb8735124389a
hungnt <tuanhung0601@live.com>
2021-05-11 00:32:17 +0700

    NUKE

--- app/compoments/SearchBar.tsx
+++ app/compoments/SearchBar.tsx
@@ -1,1 +1,35 @@
-hihi
+import React from 'react';
+import {
+  Dimensions,
+  FlatList,
+  Image,
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+  Alert,
+  Button,
+  TouchableOpacity,
+} from 'react-native';
+
+
+const {width, height} = Dimensions.get('window');
+const SearchBar = () => {
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.container}>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
+
+export default SearchBar;
